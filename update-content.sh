7a2b939a60ca16c718a4ff06e16ee7ff857aba23
Patrick M. Lenggenhager <52564853+patrick-lenggenhager@users.noreply.github.com>
2023-11-29 16:16:15 +0100

    Add beta designation to documentation

--- update-content.sh
+++ update-content.sh
@@ -1,3 +1,6 @@
+cd ..
+gap makedoc.g -c "QUIT;"
+cd gh-pages
 cp -f ../PackageInfo.g ../README* ../AUTHORS.md ../LICENSE.txt ../CONTRIBUTING.md .
 cp -f ../doc/*.{css,html,js,txt} doc/
 gap update.g
