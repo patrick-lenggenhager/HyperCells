e27765c797f563655b6ca1de94a540fc835f7f97
Patrick M. Lenggenhager <52564853+patrick-lenggenhager@users.noreply.github.com>
2023-11-29 15:41:48 +0100

    Add CONTRIBUTING.md

--- update-content.sh
+++ update-content.sh
@@ -1,3 +1,3 @@
-cp -f ../PackageInfo.g ../README* ../AUTHORS.md ../LICENSE.txt .
+cp -f ../PackageInfo.g ../README* ../AUTHORS.md ../LICENSE.txt ../CONTRIBUTING.md .
 cp -f ../doc/*.{css,html,js,txt} doc/
 gap update.g
